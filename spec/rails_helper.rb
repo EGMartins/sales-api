140f5155b35eebfe10e9d23d568ba2a3c5cefd5b
Eduardo Martins <mirassol.code@gmail.com>
2018-11-02 16:10:10 -0300

    Config the database cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,7 +67,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
   config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.clean_with(:truncation, except: %w(ar_internal_metadata))
     DatabaseCleaner.strategy = :transaction
   end
   config.around(:each) do |example|

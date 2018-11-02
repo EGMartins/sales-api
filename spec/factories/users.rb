517a806f2742492b19cb73c735c37595c43ebb61
Eduardo Martins <mirassol.code@gmail.com>
2018-11-02 18:04:54 -0300

    Adding the JWT auth system

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
-    factory :user do
-      name { Faker::Name.name }
-      email { Faker::Internet.email }
-      password { Faker::Simpsons.character }
-    end
+  factory :user do
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { Faker::Number.number(10) }
   end
+end
